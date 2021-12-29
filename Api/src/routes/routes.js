c07326101fd89e47cc4a571aa2272ba04a50c3c5
TomasGit-1 <tomaslopezperez107@gmail.com>
2021-12-29 12:44:16 -0600

    Se sube carga de archivo en el servidor

--- Api/src/routes/routes.js
+++ Api/src/routes/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { Pool  } = require('pg');
 var fs = require('fs');
-const fileUpload = require('express-fileupload');
+let json = require('../config/configApi.json');
+let rutas = json['rutas'];
+// const fileUp = require('express-fileupload');
 const routes = express.Router();
 const db = require('../controllers/controller.db.js');
 const { error } = require('console');
+const objUtil  = require('../utilities/util.js')
 routes.get('/one' , (req, res)=> {
-    
     res.send({ "name":"Testing Api"});
 });
 
@@ -36,25 +38,72 @@ routes.post('/createServicio' , (req, res)=> {
 });
 
 routes.post('/createRegistro',  (req, res )=> {
-    db.Home().then(respuesta =>{
-        // console.log(db.Home());
-        // let datos = req.body;
-        // let sampleFile;
-        // const curp = datos['curp'];
+    try {
+        let datos = req.body;
+        let sampleFile;
+        const curp = datos.curp;
     
-        // //En esta ruta se guardan los archivos pdf 
-        // var dirpdf = __dirname +  rutas[0]['upload'] + curp+'/';
-        // sampleFile = req.files.pdfcurp;
-        // save(req , dirpdf , sampleFile);
+        //En esta ruta se guardan los archivos pdf 
+        var dirpdf = __dirname +  rutas[0]['upload'] + curp+'/';
+        sampleFile = req.files.fileCurp;
+        save(req , dirpdf , sampleFile);
     
-        // //En esta ruta se guardan las fotografias 
-        // var dirimg = __dirname +  rutas[0]['images'] + curp+'/';
-        // sampleFile = req.files.imgUser;
-        // let respuesta = save(req , dirimg , sampleFile);
+        //En esta ruta se guardan las fotografias 
+        var dirimg = __dirname +  rutas[0]['images'] + curp+'/';
+        sampleFile = req.files.fileImg;
+        let respuesta = save(req , dirimg , sampleFile);
         res.json({ "status": respuesta});
-    }).catch(error =>{
-        console.log(error)
-    })
+                
+        // db.Home().then(respuesta =>{
+        //     console.log(db.Home());
+        //     let datos = req.body;
+        //     let sampleFile;
+        //     const curp = datos['curp'];
+        
+        //     //En esta ruta se guardan los archivos pdf 
+        //     var dirpdf = __dirname +  rutas[0]['upload'] + curp+'/';
+        //     sampleFile = req.files.pdfcurp;
+        //     save(req , dirpdf , sampleFile);
+        
+        //     //En esta ruta se guardan las fotografias 
+        //     var dirimg = __dirname +  rutas[0]['images'] + curp+'/';
+        //     sampleFile = req.files.imgUser;
+        //     let respuesta = save(req , dirimg , sampleFile);
+        //     res.json({ "status": respuesta});
+        // }).catch(error =>{
+        //     console.log(error)
+        // })
+    } catch (error) {
+        res.json({ "error": error.message});
+
+    }
+    
+
 });
+
+const save = ( req , dir , sampleFile) =>{
+    let uploadPath;
+    try {
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, 0744);
+        }
+        if(!req.files || Object.keys(req.files).length === 0 ){
+            console.log('No file were uploaded');
+        }
+
+        uploadPath = dir+objUtil.RandomName()+sampleFile.name;
+        sampleFile.mv(uploadPath, function(err) {
+            if (err){
+                console.log(err);
+            }
+        });
+        return 200;
+    } catch (error) {
+        return error;
+        
+    }
+
+}
+
 
 module.exports = routes;
---
+++ Api/src/utilities/util.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+// const fileUpl = require('express-fileupload');
+
+const fileUpload = (req  , path ,file ) => {
+    let uploadPath;
+    try {
+        if (!fs.existsSync(path)) {
+            fs.mkdirSync(path, 0744);
+        }
+        if(!req.files || Object.keys(req.files).length === 0 ){
+            console.log('No file were uploaded');
+        }
+        uploadPath = path+file.name;
+        file.mv(uploadPath, function(err) {
+            if (err){
+                console.log(err);
+            }else{
+                console.log("File uploaded")
+            }
+        });
+        return 200;
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
+
+const RandomName = () => {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+module.exports = {
+    RandomName
+};
